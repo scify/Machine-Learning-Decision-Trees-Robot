49625e099a862ca7cad50c367b1f73e846a320d5
Paul Isaris <paulisaris@gmail.com>
2018-10-10 14:22:19 +0300

    ESLint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jquery": true,
+    },
+
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    // https://eslint.org/docs/rules/
+    "rules": {
+        "linebreak-style": ["error", "unix"],
+        "no-console": "off"
+    }
 };
