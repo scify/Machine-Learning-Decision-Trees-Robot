affcab1c857cc63ba3d595ba95ba6f87f4d3895d
Paul Isaris <paulisaris@gmail.com>
2022-11-09 12:15:11 +0200

    Added installation node variable

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-const hostname = window.location.hostname;
-let path = "/courses/decision-trees/";
-if(hostname.includes("github"))
-    path = "/Machine-Learning-JS-playground/";
+const hostname = process.env.INSTALLATION;
+let path = "/Machine-Learning-JS-playground/";
+if(hostname.includes("ai.scify.org"))
+    path = "/courses/decision-trees/";
 
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? path : "/",
