1fbd2d7d43fa612b832312692ff68a9bfad091e3
Paul Isaris <paulisaris@gmail.com>
2022-11-09 12:10:50 +0200

    - Updated app homepage in package.json - Updated vue.config.js to use a path based on the domain name

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const hostname = window.location.hostname;
+let path = "/courses/decision-trees/";
+if(hostname.includes("github"))
+    path = "/Machine-Learning-JS-playground/";
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/Machine-Learning-JS-playground/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? path : "/",
 };
