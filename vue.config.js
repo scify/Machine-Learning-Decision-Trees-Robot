91f246a2ce6d02dafea9115b1d975136a96375de
Paul Isaris <paulisaris@gmail.com>
2022-11-12 11:33:58 +0200

    Updated GitHub path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const hostname = process.env.INSTALLATION;
-let path = "/Machine-Learning-JS-playground/";
+let path = "/Machine-Learning-Decision-Trees-Robot/";
 if(hostname && hostname.includes("ai.scify.org"))
     path = "/courses/decision-trees/";
 
